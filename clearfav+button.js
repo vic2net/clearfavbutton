b0286db593587b5d138c74444c0cf7f71141c336
Vic <vic2net@gmail.com>
2026-06-21 00:50:47 +0300

    Update clearfav+button.js

--- clearfav+button.js
+++ clearfav+button.js
@@ -31,14 +31,17 @@
     function clearHistory() {
         var history = Lampa.Storage.get('history', {});
 
-        if (Array.isArray(history)) history = [];
-        else if (history && typeof history == 'object') {
+        if (Array.isArray(history)) {
+            history = [];
+        } else if (history && typeof history == 'object') {
             Object.keys(history).forEach(function (key) {
                 if (Array.isArray(history[key])) history[key] = [];
                 else if (history[key] && typeof history[key] == 'object') history[key] = {};
                 else delete history[key];
             });
-        } else history = {};
+        } else {
+            history = {};
+        }
 
         Lampa.Storage.set('history', history);
     }
@@ -54,16 +57,19 @@
 
         Lampa.SettingsApi.addComponent({
             component: pluginId,
-            name: 'Очистка',
+            name: 'Очистка истории',
             icon: '<svg height="260" viewBox="0 0 244 260" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M40 78h164v156c0 14-12 26-26 26H66c-14 0-26-12-26-26V78zm38-48h88l12 22h52v26H14V52h52l12-22zm18 82v102h20V112H96zm48 0v102h20V112h-20z" fill="white"/></svg>'
         });
 
         Lampa.SettingsApi.addParam({
             component: pluginId,
             param: {
+                name: pluginId + '_clear',
                 type: 'button',
-                component: pluginId + '_clear',
-                name: 'Отчистить историю и избранное'
+                default: false
+            },
+            field: {
+                name: 'Очистить историю и избранное'
             },
             onChange: clearAll
         });
@@ -71,8 +77,11 @@
         Lampa.SettingsApi.addParam({
             component: pluginId,
             param: {
+                name: pluginId + '_no',
                 type: 'button',
-                component: pluginId + '_no',
+                default: false
+            },
+            field: {
                 name: 'Нет'
             },
             onChange: function () {
