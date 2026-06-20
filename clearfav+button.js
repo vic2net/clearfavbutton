9ce66bfe7b4d2cf6401fe9f050154499ca342525
Vic <vic2net@gmail.com>
2026-06-21 01:20:58 +0300

    Update clearfav+button.js

--- clearfav+button.js
+++ clearfav+button.js
@@ -52,16 +52,6 @@
         notify('История и избранное очищены');
     }
 
-    function clearOnlyHistory() {
-        clearHistory();
-        notify('История просмотров очищена');
-    }
-
-    function clearOnlyFavorite() {
-        clearFavorite();
-        notify('Избранное очищено');
-    }
-
     function createSettings() {
         if (!Lampa.SettingsApi) return;
 
@@ -69,32 +59,6 @@
             component: pluginId,
             name: 'Очистка истории',
             icon: '<svg height="260" viewBox="0 0 244 260" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M40 78h164v156c0 14-12 26-26 26H66c-14 0-26-12-26-26V78zm38-48h88l12 22h52v26H14V52h52l12-22zm18 82v102h20V112H96zm48 0v102h20V112h-20z" fill="white"/></svg>'
-        });
-
-        Lampa.SettingsApi.addParam({
-            component: pluginId,
-            param: {
-                name: pluginId + '_clear_history',
-                type: 'button',
-                default: false
-            },
-            field: {
-                name: 'Очистка истории просмотров'
-            },
-            onChange: clearOnlyHistory
-        });
-
-        Lampa.SettingsApi.addParam({
-            component: pluginId,
-            param: {
-                name: pluginId + '_clear_favorite',
-                type: 'button',
-                default: false
-            },
-            field: {
-                name: 'Очистка вкладки Избранное'
-            },
-            onChange: clearOnlyFavorite
         });
 
         Lampa.SettingsApi.addParam({
